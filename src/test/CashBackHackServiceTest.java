83c19ac18b5bc00caf37a943b2ced2418e50aa67
Юлия Ц <107360558+JulietteT@users.noreply.github.com>
2023-04-02 18:25:04 +0500

    Update CashBackHackServiceTest.java

--- src/test/CashBackHackServiceTest.java
+++ src/test/CashBackHackServiceTest.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class CashBackHackServiceTest {
+package ru.netology.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+public class CashbackHackServiceTest {
+    CashbackHackService service = new CashbackHackService();
+
+
+    @Test
+    void shouldReturn100IfAmountIs900() {
+
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+        int expected = 100;
+
+        int actual = service.remain(amount);
+
+        assertEquals(expected, actual);
+    }
 }
